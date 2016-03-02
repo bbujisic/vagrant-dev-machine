993246b433a8ab3c774b69e66619bd8072060df2
Branislav Bujisic <branislav.bujisic@ubgnet.de>
2016-03-02 12:10:13 +0100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,21 @@
+# Install host manager
+vagrant plugin install vagrant-hostmanager
+
+# Install proxy
+vagrant plugin install vagrant-proxyconf
+
+# Start your vagrant
+vagrant up
+
+# Wanna ssh?
+vagrant ssh
+
+# Wanna shut down the machine?
+vagrant halt
+
+# Wanna remove the machine?
+vagrant destroy
+
+
+Have fun!
+Branislav
---
+++ scripts/provision/WebDev.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Variables
+APPENV=local
+DBHOST=localhost
+DBNAME=db
+DBUSER=root
+DBPASSWD=root
+
+#  > /dev/null 2>&1
+
+echo -e "\n--- Provisioning virtual machine. Go get a coffee, this will take a while ---\n"
+
+mkdir -p /var/www
+sudo apt-get update > /dev/null 2>&1
+
+echo -e "\n--- Downloading and setting up LAMP stack ---\n"
+
+# This is needed in order for pecl uploadprogress to be installed.
+sudo apt-get install -y build-essential curl > /dev/null 2>&1
+sudo apt-get install -y python-software-properties > /dev/null 2>&1
+sudo add-apt-repository -y ppa:brianmercer/php5-xhprof > /dev/null 2>&1
+sudo add-apt-repository -y ppa:brightbox/ruby-ng > /dev/null 2>&1
+sudo add-apt-repository -y ppa:chris-lea/node.js > /dev/null 2>&1
+sudo apt-get update > /dev/null 2>&1
+sudo add-apt-repository -y ppa:ondrej/php5-5.6
+sudo apt-get update > /dev/null 2>&1
+
+echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
+apt-get -y install mysql-server-5.5 mysql-common mysql-client > /dev/null 2>&1
+
+sudo apt-get install -y apache2 > /dev/null 2>&1
+sudo apt-get install -y php5 libapache2-mod-php5 php5-mhash php5-mcrypt php5-curl php5-cli php5-mysql php5-gd php5-intl php5-xsl > /dev/null 2>&1
+sudo apt-get install -y php-pear php5-dev php5-memcache php-apc php5-xdebug graphviz > /dev/null 2>&1
+
+echo -e "\n--- Downloading and setting up Git ---\n"
+sudo apt-get install -y git > /dev/null 2>&1
+
+echo -e "\n--- Downloading and setting up PhpMyAdmin ---\n"
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
+sudo apt-get install -y phpmyadmin > /dev/null 2>&1
+
+echo -e "\n--- Downloading and setting up Memcache ---\n"
+sudo aptitude install -y memcached > /dev/null 2>&1
+pecl install -y memcached > /dev/null 2>&1
+
+echo -e "\n--- Downloading and setting up UploadProgress ---\n"
+sudo pecl install -Z uploadprogress > /dev/null 2>&1
+
+echo -e "\n--- Creating MySQL root user and db ---\n"
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'"
+
+echo -e "\n--- Finalizing setup ---\n"
+sudo apt-get upgrade > /dev/null 2>&1
+
+echo "short_open_tag = On" >> /etc/php5/apache2/php.ini
+echo "display_errors = On" >> /etc/php5/apache2/php.ini
+echo "display_startup_errors = On" >> /etc/php5/apache2/php.ini
+echo "error_log = '/var/log/php-errors.log'" >> /etc/php5/apache2/php.ini
+echo "max_execution_time = 90" >> /etc/php5/apache2/php.ini
+echo "post_max_size = 1024M" >> /etc/php5/apache2/php.ini
+echo "upload_max_filesize = 1024M" >> /etc/php5/apache2/php.ini
+echo "extension=memcache.so" >> /etc/php5/apache2/php.ini
+echo "extension=uploadprogress.so" >> /etc/php5/apache2/php.ini
+
+# @todo:  /etc/php5/conf.d/xdebug.ini: No such file or directory
+echo "xdebug.remote_enable = 1" >> /etc/php5/conf.d/xdebug.ini
+echo "xdebug.remote_connect_back = 1" >> /etc/php5/conf.d/xdebug.ini
+
+echo "<Directory /var/www/html>" >> /etc/apache2/apache2.conf
+echo "  Options -Indexes +FollowSymLinks" >> /etc/apache2/apache2.conf
+echo "  AllowOverride All" >> /etc/apache2/apache2.conf
+echo "</Directory>" >> /etc/apache2/apache2.conf
+
+sudo a2enmod rewrite
+
+
+
+service apache2 restart
